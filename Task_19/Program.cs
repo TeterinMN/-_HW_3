28945ad6bfb82801f1c1e3f6bfe0a75fd62508fe
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-15 22:58:24 +0900

    Написал код к задаче №19

---
+++ Task_19/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.Clear();
+System.Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(System.Console.ReadLine()!);
+int reverseNumber = 0;
+int temp = number;
+
+while (number > 0)
+{
+    int digit = number % 10;
+    reverseNumber = reverseNumber * 10 + digit;
+    number /= 10;
+}
+if (temp >= 10000 && temp <= 99999)
+{
+    if (temp == reverseNumber)
+    {
+        System.Console.WriteLine($"Число {temp} - является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {temp} - не является палиндромом");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено некорректное число");
+}
